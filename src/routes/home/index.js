f219c8b31833ee942cab4063a016a4c531f82e5d
Facundo Alexandre <facualex@gmail.com>
2020-09-08 18:03:22 -0300

    Funcion innecesaria

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -44,11 +44,6 @@ function getMatrixEncoding(matrixState) {
   return encodedJson
 }
 
-const random_hex_color_code = () => {
-  const n = (Math.random() * 0xfffff * 1000000).toString(16)
-  return `#${n.slice(0, 6)}`
-}
-
 function getRandomHexColor() {
   const hexNumber = (Math.random() * 0xffffff * 1000000).toString(16)
   return `#${hexNumber.slice(0, 6)}`
